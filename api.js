655dd6b0bd9f5e6a36bb7a55cb03e8b334369b6f
Darren Lim <darrenlim193@gmail.com>
2019-02-04 13:21:09 +1100

    Added git ignore

--- api.js
+++ api.js
@@ -1,8 +1,6 @@
 const express = require('express')
-const axios = require('axios');
 const mongoose = require('mongoose')
 const cors = require('cors')
-const mortgage = require('./models/mortgage');
 require('dotenv').config();
 
 //Start Express
