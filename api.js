ca6eb5f1c28b7710700244bfb4a59dabdb76343a
Darren Lim <darrenlim193@gmail.com>
2019-02-10 14:13:32 +1100

    Minor changes

--- api.js
+++ api.js
@@ -20,10 +20,7 @@ mongoose.connection.on('error', () => {
     console.log('Failed to connect to mongod');
   });
 
-app.use(cors({
-    credentials: true,
-    origin: process.env.REQUEST_ORIGIN
-  }));
+app.use(cors());
 app.use(express.json())
 app.use(require('./controllers'));
 
